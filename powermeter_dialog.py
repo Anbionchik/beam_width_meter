bbebd6d86b4399d778f64037b780900a032e59bb
Anbion <khlopkov@melsytech.com>
2023-12-07 13:22:31 +0300

    Добавлен чек-бокс Авто в диалог выбора измерителя

--- powermeter_dialog.py
+++ powermeter_dialog.py
@@ -2,13 +2,11 @@
 
 # Form implementation generated from reading ui file 'powermeter_dialog.ui'
 #
-# Created by: PyQt5 UI code generator 5.12.3
+# Created by: PyQt5 UI code generator 5.9.2
 #
 # WARNING! All changes made in this file will be lost!
 
-
 from PyQt5 import QtCore, QtGui, QtWidgets
-
 
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
@@ -16,10 +14,11 @@ class Ui_Dialog(object):
         Dialog.resize(330, 247)
         self.radioButton = QtWidgets.QRadioButton(Dialog)
         self.radioButton.setGeometry(QtCore.QRect(20, 10, 82, 17))
-        self.radioButton.setChecked(True)
+        self.radioButton.setChecked(False)
         self.radioButton.setObjectName("radioButton")
         self.radioButton_2 = QtWidgets.QRadioButton(Dialog)
         self.radioButton_2.setGeometry(QtCore.QRect(20, 90, 82, 17))
+        self.radioButton_2.setChecked(True)
         self.radioButton_2.setObjectName("radioButton_2")
         self.layoutWidget = QtWidgets.QWidget(Dialog)
         self.layoutWidget.setGeometry(QtCore.QRect(20, 30, 191, 48))
@@ -33,6 +32,7 @@ class Ui_Dialog(object):
         self.label.setObjectName("label")
         self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
         self.ip_line = QtWidgets.QLineEdit(self.layoutWidget)
+        self.ip_line.setEnabled(True)
         self.ip_line.setMinimumSize(QtCore.QSize(140, 0))
         self.ip_line.setObjectName("ip_line")
         self.gridLayout.addWidget(self.ip_line, 0, 1, 1, 1)
@@ -41,19 +41,26 @@ class Ui_Dialog(object):
         self.label_2.setObjectName("label_2")
         self.gridLayout.addWidget(self.label_2, 1, 0, 1, 1)
         self.port_line = QtWidgets.QLineEdit(self.layoutWidget)
+        self.port_line.setEnabled(True)
         self.port_line.setMinimumSize(QtCore.QSize(140, 0))
         self.port_line.setObjectName("port_line")
         self.gridLayout.addWidget(self.port_line, 1, 1, 1, 1)
         self.layoutWidget1 = QtWidgets.QWidget(Dialog)
-        self.layoutWidget1.setGeometry(QtCore.QRect(20, 110, 193, 79))
+        self.layoutWidget1.setGeometry(QtCore.QRect(20, 110, 193, 102))
         self.layoutWidget1.setObjectName("layoutWidget1")
         self.verticalLayout = QtWidgets.QVBoxLayout(self.layoutWidget1)
         self.verticalLayout.setContentsMargins(0, 0, 0, 0)
         self.verticalLayout.setObjectName("verticalLayout")
+        self.checkBox = QtWidgets.QCheckBox(self.layoutWidget1)
+        self.checkBox.setObjectName("checkBox")
+        self.verticalLayout.addWidget(self.checkBox)
         self.com_port_box = QtWidgets.QComboBox(self.layoutWidget1)
+        self.com_port_box.setEnabled(True)
+        self.com_port_box.setEditable(True)
         self.com_port_box.setObjectName("com_port_box")
         self.verticalLayout.addWidget(self.com_port_box)
         self.rescan_btn = QtWidgets.QPushButton(self.layoutWidget1)
+        self.rescan_btn.setEnabled(True)
         self.rescan_btn.setMaximumSize(QtCore.QSize(75, 23))
         self.rescan_btn.setObjectName("rescan_btn")
         self.verticalLayout.addWidget(self.rescan_btn)
@@ -83,7 +90,9 @@ class Ui_Dialog(object):
         self.radioButton_2.setText(_translate("Dialog", "USB"))
         self.label.setText(_translate("Dialog", "IP"))
         self.label_2.setText(_translate("Dialog", "Port"))
+        self.checkBox.setText(_translate("Dialog", "Авто"))
         self.rescan_btn.setText(_translate("Dialog", "Rescan"))
         self.label_3.setText(_translate("Dialog", "Baud_rate"))
         self.ok_btn.setText(_translate("Dialog", "OK"))
         self.cancel_btn.setText(_translate("Dialog", "Отмена"))
+
